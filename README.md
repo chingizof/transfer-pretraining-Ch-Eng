48a70a2b3f963403177501710964469aab0c5068
Chingiz <chingizof@gmail.com>
2024-02-29 11:57:25 -0500

    added russian language model from huggingface

--- README.md
+++ README.md
@@ -1,3 +1,6 @@
 # transfer-pretraining-Ch-Eng
 
 This repository hosts an English-Cherokee Neural Machine Translation (NMT) model, leveraging advanced knowledge transfer techniques from a pre-trained Russian-English model. Both the encoder and decoder components of our model have been enriched with linguistic features and structural insights gained from the Russian-English language pair, aiming to enhance translation quality and fluency for the low-resource Cherokee language. This approach not only addresses the challenge of limited Cherokee-English parallel corpora but also sets a new precedent for leveraging cross-lingual transfer learning in NMT systems.
+
+
+You can save russian model in ~/hugging-face/model folder, for that you need to uncomment several lines at the beginning of '~/hugging-face/main.py'
---
+++ hugging-face/main.py
@@ -0,0 +1,33 @@
+from transformers import AutoTokenizer, AutoModelForSeq2SeqLM
+
+#if your model is saved in "model" folder
+# tokenizer = AutoTokenizer.from_pretrained("./model/")
+# model = AutoModelForSeq2SeqLM.from_pretrained("./model/")
+
+tokenizer = AutoTokenizer.from_pretrained("Helsinki-NLP/opus-mt-ru-en")
+model = AutoModelForSeq2SeqLM.from_pretrained("Helsinki-NLP/opus-mt-ru-en")
+
+# Save the model and tokenizer to a directory
+# model.save_pretrained('./model')
+# tokenizer.save_pretrained('./model/')
+
+
+
+def translate(text, model, tokenizer):
+    # Tokenize the input text and return PyTorch tensors
+    inputs = tokenizer.encode(text, return_tensors="pt")
+    
+    # Generate translation using model
+    outputs = model.generate(inputs, max_length=40, num_beams=4, early_stopping=True)
+    
+    # Decode the generated tokens to string
+    translated_text = tokenizer.decode(outputs[0], skip_special_tokens=True)
+    
+    return translated_text
+
+
+russian_text = "Привет, как дела?"
+translated_text = translate(russian_text, model, tokenizer)
+print(f"Translated Text: {translated_text}")
+
+
